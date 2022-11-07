422231586ae5bf5066a27646710acfbb466c8b5b
Buddhi Thilakshana <hellobachchi@gmail.com>
2022-11-07 09:34:29 +0530

    yui

--- my_bot.py
+++ my_bot.py
@@ -53,7 +53,7 @@ emoj = re.compile("["
                   u"\ufe0f"  # dingbats
                   u"\u3030"
                   "]+", re.UNICODE)
-to_channel_id = -1001834671869
+to_channel_id = -1001413593587
 
 
 def is_english(text):
