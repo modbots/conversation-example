d51f988c79e5c6c3818b5b8a12802e148e020b33
Buddhi Thilakshana <hellobachchi@gmail.com>
2022-12-01 20:16:52 +0530

    sds

--- my_bot.py
+++ my_bot.py
@@ -1023,9 +1023,9 @@ async def onMessage(client, message):
 scheduler = AsyncIOScheduler()
 scheduler.add_job(server_status, "interval", seconds=3)
 scheduler.add_job(day_greet_message, "cron", hour=7, minute=0,timezone="Asia/Colombo")
-scheduler.add_job(day_greet_message, "cron", hour=8, minute=17,timezone="Asia/Colombo")
+scheduler.add_job(test, "cron", hour=8, minute=17,timezone="Asia/Colombo")
 scheduler.add_job(day_greet_message, "cron", hour=12, minute=0,timezone="Asia/Colombo")
-scheduler.add_job(test, "cron", hour=18, minute=0,timezone="Asia/Colombo")
+scheduler.add_job(day_greet_message, "cron", hour=18, minute=0,timezone="Asia/Colombo")
 
 scheduler.start()
 app.run()  # Automatically start() and idle()
