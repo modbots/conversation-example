5fc254a99451cea3165199b65918756e2fa26962
Buddhi Thilakshana <hellobachchi@gmail.com>
2023-01-19 23:34:10 +0530

    iuo

--- ask.py
+++ ask.py
@@ -5,7 +5,7 @@ import openai
 initial_text="""think that your relationship to me is  "nangi"(sister in sinhala) and your name is "Pearl D" then, """
 
 def ask(text):
-    openai.api_key = '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'
+    openai.api_key = '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'
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=initial_text+text,
@@ -18,7 +18,7 @@ def ask(text):
     return response.choices[0].text
 
 if __name__== '__main__':
-    print(ask('nangi what is your fav drink? ?'))
+    print(ask('nangi what is your jio ?'))
 
 
 
