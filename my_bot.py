c10264e5feb1e331be2dd901f1390e1ba36b0bce
Buddhi Thilakshana <hellobachchi@gmail.com>
2023-01-22 00:52:45 +0530

    sub

--- my_bot.py
+++ my_bot.py
@@ -757,7 +757,8 @@ async def onMessage(client, message):
                 await message.reply_text('මට චුට්ටක් ඔලුව රිදෙනවා වගේ තවටිකකින් අහන්න ♥')
                 return
         
-        caption = caption.replace("nangi", "").replace("Nangi", "")
+        #replace the first word
+        caption = re.sub(r'^\w+\s*', '', caption)
         if "image" in caption or "art" in caption or "photo" in caption or "draw" in caption or "drawing" in caption or "picture" in caption or "pic" in caption or "paint" in caption or "painting" in caption:
             await client.send_chat_action(chat_id, enums.ChatAction.UPLOAD_PHOTO)
             await message.reply_photo(openai.generateImage(question=caption))
